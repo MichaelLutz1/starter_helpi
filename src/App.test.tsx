8777a3d42b935dd03b8185c49997d66c657a4e60
jacobwilbe <jacobwilbe@users.noreply.github.com>
2024-05-08 17:34:30 -0400

    fixed typo

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { HomeScreen } from './Components/HomeScreen/HomeScreen';
+
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Discover/i);
   expect(linkElement).toBeInTheDocument();
 });
+
--- src/Components/HomeScreen/HomeScreen.test.tsx
+++ src/Components/HomeScreen/HomeScreen.test.tsx
@@ -3,17 +3,17 @@ import { render, screen } from "@testing-library/react";
 import { HomeScreen } from "./HomeScreen";
 
 
-describe("ChangeType Component tests", () => {
-    beforeEach(() => {
-        // eslint-disable-next-line testing-library/no-render-in-setup
+describe("HomeScreen Component tests", () => {
+    test("The HomePage contains both buttons", () => {
         render(<HomeScreen page={"Home"} setPage={function (newPage: string): void {
             throw new Error("Function not implemented.");
         } } />);
-    });
-    test("The HomePage contains both buttons", () => {
         expect(screen.getAllByRole("button")).toHaveLength(2);
     });
     test("The HomePage mentions both basic and detailed page", () => {
+        render(<HomeScreen page={"Home"} setPage={function (newPage: string): void {
+            throw new Error("Function not implemented.");
+        } } />);
         expect(screen.getByText(/Basic Quiz/i)).toBeInTheDocument();
         expect(screen.getByText(/Detailed Quiz/i)).toBeInTheDocument();
     })
