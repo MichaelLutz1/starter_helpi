24ae06c683ec6a3808b4c5d960ed476e9c587340
jacobwilbe <jacobwilbe@users.noreply.github.com>
2024-05-08 20:20:53 -0400

    added prop to homescreen test

--- src/Components/HomeScreen/HomeScreen.test.tsx
+++ src/Components/HomeScreen/HomeScreen.test.tsx
@@ -7,13 +7,13 @@ describe("HomeScreen Component tests", () => {
     test("The HomePage contains both buttons", () => {
         render(<HomeScreen page={"Home"} setPage={function (newPage: string): void {
             throw new Error("Function not implemented.");
-        } } />);
+        } } apiKey={""} />);
         expect(screen.getAllByRole("button")).toHaveLength(2);
     });
     test("The HomePage mentions both basic and detailed page", () => {
         render(<HomeScreen page={"Home"} setPage={function (newPage: string): void {
             throw new Error("Function not implemented.");
-        } } />);
+        } } apiKey={""} />);
         expect(screen.getByText(/Basic Quiz/i)).toBeInTheDocument();
         expect(screen.getByText(/Detailed Quiz/i)).toBeInTheDocument();
     })
