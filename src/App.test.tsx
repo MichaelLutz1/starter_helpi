312dcc313328ded1f4d1cb200fc2971f0b0c281f
jacobwilbe <jacobwilbe@users.noreply.github.com>
2024-05-09 21:36:01 -0400

    finished test for basic,detail,and homepage.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import { HomeScreen } from './Components/HomeScreen/HomeScreen';
+
 
 
 test('renders learn react link', () => {
